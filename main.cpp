a09023c6fe03a5b4caa2b2b45f8f408269fe6b23
Rguzman41 <123515531+Rguzman41@users.noreply.github.com>
2023-09-20 11:34:37 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ int main(){
         if( input.size() != RGB_HEX_LENGTH ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
+        if(input[0] != '#'){
+            std::cout << "Please put # first when entering the color in hexadecimal format\n;
+        }
+        
     }while( input.size() != RGB_HEX_LENGTH );
 
     std::cout << "Your hex color is: " << input << std::endl;
